fd36e94e59f7dcdc353da14cc7d8ae4e77bcbaf6
Osmar Tomasi <osmasi@msn.com>
2023-10-06 17:37:43 -0300

    VitaLab 1.2.0

--- exames/views.py
+++ exames/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import TiposExames, PedidosExames, SolicitacaoExame
+from .models import TiposExames, PedidosExames, SolicitacaoExame, AcessoMedico
 from django.contrib import messages
 from django.contrib.messages import constants
 from datetime import datetime
@@ -76,23 +76,73 @@ def gerenciar_exames(request):
 @login_required
 def permitir_abrir_exame(request, exame_id):
     exame = SolicitacaoExame.objects.get(id=exame_id)
+    exames = SolicitacaoExame.objects.filter(usuario=request.user)
 		# validar se o exame é do usuário
+    if not exame.usuario == request.user:
+        messages.add_message(request, constants.ERROR, 'Esse exame não é seu')
+        return render(request, 'gerenciar_exames.html', {'exames': exames})
     if not exame.requer_senha:
         # verificar se o pdf existe
-        return redirect(exame.resultado.url)
+        if exame.resultado:
+            return redirect(exame.resultado.url)
+        else:
+            messages.add_message(request, constants.ERROR, 'Documento inacessível, tente novamente mais tarde.')
+            return render(request, 'gerenciar_exames.html', {'exames': exames})
     else: 
         return redirect(f'/exames/solicitar_senha_exame/{exame.id}')
     
 @login_required
 def solicitar_senha_exame(request, exame_id):
     exame = SolicitacaoExame.objects.get(id=exame_id)
+    exames = SolicitacaoExame.objects.filter(usuario=request.user)
     if request.method == "GET":
         return render(request, 'solicitar_senha_exame.html', {'exame': exame})
     elif request.method == "POST":
         senha = request.POST.get("senha")
-				# validar se o exame é do usuário
+		# validar se o exame é do usuário
+        if not exame.usuario == request.user:
+            messages.add_message(request, constants.ERROR, 'Esse exame não é seu')
+            return render(request, 'gerenciar_exames.html', {'exames': exames})
         if senha == exame.senha:
-            return redirect(exame.resultado.url)
+            # verificar se o pdf existe
+            if exame.resultado:
+                return redirect(exame.resultado.url)
+            else:
+                messages.add_message(request, constants.ERROR, 'Documento inacessível, tente novamente mais tarde.')
+                return redirect(f'/exames/solicitar_senha_exame/{exame.id}')
         else:
             messages.add_message(request, constants.ERROR, 'Senha inválida')
             return redirect(f'/exames/solicitar_senha_exame/{exame.id}')
+        
+@login_required
+def gerar_acesso_medico(request):
+    if request.method == "GET":
+        acessos_medicos = AcessoMedico.objects.filter(usuario =request.user)
+        return render(request, 'gerar_acesso_medico.html', {'acessos_medicos': acessos_medicos})
+    elif request.method == "POST":
+        identificacao = request.POST.get('identificacao')
+        tempo_de_acesso = request.POST.get('tempo_de_acesso')
+        data_exame_inicial = request.POST.get("data_exame_inicial")
+        data_exame_final = request.POST.get("data_exame_final")
+
+        acesso_medico = AcessoMedico(
+            usuario = request.user,
+            identificacao = identificacao,
+            tempo_de_acesso = tempo_de_acesso,
+            data_exames_iniciais = data_exame_inicial,
+            data_exames_finais = data_exame_final,
+            criado_em = datetime.now()
+        )
+
+        acesso_medico.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Acesso gerado com sucesso')
+        return redirect('/exames/gerar_acesso_medico')
+    
+def acesso_medico(request, token):
+    acesso_medico = AcessoMedico.objects.get(token = token)
+    if acesso_medico.status == 'Expirado':
+        messages.add_message(request, constants.WARNING, 'Esse link já se expirou!')
+        return redirect('/usuarios/login')
+    pedidos = PedidosExames.objects.filter(data__gte = acesso_medico.data_exames_iniciais).filter(data__lte = acesso_medico.data_exames_finais).filter(usuario=acesso_medico.usuario)
+    return render(request, 'acesso_medico.html', {'pedidos': pedidos})
